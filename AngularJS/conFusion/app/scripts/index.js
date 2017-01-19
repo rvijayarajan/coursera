bb8e4bc7354e78057df3006c5b39fd2581dd72d3
rvijayarajan <viravindran@inautix.co.in>
2017-01-19 21:13:06 +0530

    FormExcercise

--- AngularJS/conFusion/app/scripts/index.js
+++ AngularJS/conFusion/app/scripts/index.js
@@ -70,3 +70,37 @@ app.controller('MenuController', function($scope) {
         $scope.showDetails = !$scope.showDetails;
     };
 });
+
+app.controller('ContactController', function ($scope) {
+    $scope.feedback = {
+        mychannel: "",
+        firstName: "",
+        lastName: "",
+        agree: false,
+        email: ""
+    };
+	var channels = [
+		{value:"tel", label:"Tel."}, 
+		{value:"Email",label:"Email"}
+	];
+	$scope.channels = channels;
+	$scope.invalidChannelSelection = false;
+});
+
+app.controller('FeedbackController', function ($scope) {
+    $scope.sendFeedback = function() {
+		console.log($scope.feedback);
+		if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+			$scope.invalidChannelSelection = true;
+			console.log('incorrect');
+		}
+		else {
+			$scope.invalidChannelSelection = false;
+			$scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+			$scope.feedback.mychannel="";
+
+			$scope.feedbackForm.$setPristine();
+			console.log($scope.feedback);
+		}
+	};
+});
